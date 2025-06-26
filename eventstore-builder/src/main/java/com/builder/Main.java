e4c13e16c0b4c8096d09a4f5fbfc55cb54af1cc2
LauraFernandezBatista <laufb2004@gmail.com>
2025-06-26 20:48:14 +0100

    Main de eventstorebuilder creado

--- eventstore-builder/src/main/java/com/builder/Main.java
+++ eventstore-builder/src/main/java/com/builder/Main.java
@@ -1,7 +1,13 @@
 package com.builder;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String[] topics = {"News", "YouTube"};
+
+        EventStoreSubscriber subscriber = new EventStoreSubscriber();
+        subscriber.start(topics);
+
+        System.out.println("EventStoreBuilder activo. Ctrl+C para salir.");
+        subscriber.waitForever();
     }
 }
